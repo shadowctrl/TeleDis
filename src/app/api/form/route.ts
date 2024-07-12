5fe74220377c8fb068e0f280f1272d91617e65a5
Shadowctrl <shadowctrl@outlook.com>
2024-07-12 23:06:42 +0530

    API endpoint Form submit

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+import path from "path";
+interface RequestData {
+  telegramId: string;
+  discordId: string;
+}
+const saveSettings = async (data: RequestData): Promise<void> => {
+  const filePath = path.join(process.cwd(), "src/lib/formData.json");
+  await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+};
 
 export const POST = async (request: Request) => {
   const res = await request.json();
-
+  await saveSettings(res);
   return new Response("successful", { status: 200 });
 };
