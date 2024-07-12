1ac605a0201bbb83d4ee713ad801d7169798cab6
Shadowctrl <shadowctrl@outlook.com>
2024-07-12 15:15:07 +0530

    Settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,6 +1,52 @@
+"use client";
+import { FormEvent, useState } from "react";
 import "./settings.css";
 import { Settings2Icon } from "lucide-react";
+
 const page = () => {
+  const [submitted, setSubmit] = useState(false);
+  const [success, setSuccess] = useState<boolean | undefined>(undefined);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSubmit(true);
+    const formData = new FormData(event.currentTarget);
+    const rawFormData = {
+      telegramId: formData.get("Tid"),
+      discordId: formData.get("Did"),
+    };
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(rawFormData),
+    };
+
+    try {
+      const res = await fetch("http://localhost:3000/api/form", options);
+
+      if (res.ok) {
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(undefined);
+        }, 3000);
+        event.currentTarget?.reset();
+      } else {
+        setSuccess(false);
+        setTimeout(() => {
+          setSuccess(undefined);
+        }, 3000);
+        event.currentTarget?.reset();
+      }
+    } catch (err) {
+      setSuccess(false);
+      setTimeout(() => {
+        setSuccess(undefined);
+      }, 3000);
+      event.currentTarget?.reset();
+    } finally {
+      setSubmit(false);
+    }
+  };
   return (
     <div className="settings-main">
       <div className="settings-form">
@@ -8,7 +54,7 @@ const page = () => {
           settings <Settings2Icon size={"5vw"} />
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="Tid">Telegram Channel ID/Name</label>
             <input
@@ -16,6 +62,8 @@ const page = () => {
               id="Tid"
               name="Tid"
               placeholder="@mirrorbot / -1020131887"
+              required
+              autoComplete="off"
             />
           </div>
           <div>
@@ -25,10 +73,24 @@ const page = () => {
               id="Did"
               name="Did"
               placeholder="@mirrorbot / -1020131887"
+              required
+              autoComplete="off"
             />
           </div>
+          {success === undefined && (
+            <input
+              type="submit"
+              value="Submit"
+              className="settings-form-submit"
+              disabled={submitted}
+            />
+          )}
         </form>
-        <input type="submit" value="Submit" className="settings-form-submit" />
+
+        {success && (
+          <p className="form-success">settings applied successfully!</p>
+        )}
+        {success === false && <p>Save settings Failed</p>}
       </div>
     </div>
   );
