390dd578a6dff7a6c4af7fdabf4cd9af1fa9f01e
Shadowctrl <shadowctrl@outlook.com>
2024-07-16 16:16:51 +0530

    Dashboard shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
-import ToggleSwitch from "@/components/switch";
-
+import Charts from "@/components/charts";
 const Timer = dynamic(() => import("@/lib/timer"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
@@ -8,15 +7,34 @@ const Timer = dynamic(() => import("@/lib/timer"), {
 
 export default function Home() {
   return (
-    <main>
+    <main className="App-main">
       <div className="hero-head">
         <h1 className="hero-title">Dashboard</h1>
+        <h1 className="hero-time">
+          <Timer />
+        </h1>
+      </div>
+      <div className="hero-head-cards">
         <div>
-          <h1 className="hero-time">
-            <Timer />
-          </h1>
-          <ToggleSwitch />
+          <h2 className="hero-card-title">Total Messages</h2>
+          <p>10000</p>
         </div>
+        <div>
+          <h2 className="hero-card-title">Error Rates</h2>
+          <p>0</p>
+        </div>
+        <div>
+          <h2 className="hero-card-title">Last Message ID</h2>
+          <p>0</p>
+        </div>
+        <div>
+          <h2 className="hero-card-title">Logs</h2>
+          <p>0</p>
+        </div>
+      </div>
+      <div className="hero-chart">
+        <h2> Overview</h2>
+        <Charts />
       </div>
     </main>
   );
---
+++ src/components/charts.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "./ui/chart";
+
+const Charts = () => {
+  const chartData = [
+    { month: "January", messages: 186 },
+    { month: "February", messages: 305 },
+    { month: "March", messages: 237 },
+    { month: "April", messages: 73 },
+    { month: "May", messages: 209 },
+    { month: "June", messages: 214 },
+    { month: "July", messages: 214 },
+    { month: "August", messages: 500 },
+    { month: "September", messages: 800 },
+    { month: "October", messages: 430 },
+    { month: "November", messages: 700 },
+    { month: "December", messages: 245 },
+  ];
+
+  const chartConfig = {
+    messages: {
+      label: "Total Messages",
+      color: "#ffffff",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <ChartContainer config={chartConfig} className="w-[45vw] h-[50vh]">
+      <BarChart accessibilityLayer data={chartData}>
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <YAxis
+          dataKey="messages"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+        />
+        <ChartTooltip
+          content={
+            <ChartTooltipContent
+              className="chart-tooltip"
+              color="white"
+              indicator="line"
+            />
+          }
+        />
+        <ChartLegend content={<ChartLegendContent className="text-white" />} />
+        <Bar dataKey={"messages"} radius={4} fill="white" />
+      </BarChart>
+    </ChartContainer>
+  );
+};
+
+export default Charts;
